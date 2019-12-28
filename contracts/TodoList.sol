3a969a6a989a03334adbdc5ba7a6f9058c8bcc6e
Jayamaruthyraman Jaganathan <jjaganathan@deloitte.com>
2019-12-28 11:57:35 -0500

    second commit

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,20 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
-    
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        Task memory _task = Task(taskCount, _content, false);
+        tasks[taskCount] = _task;
+    }
+
+    constructor() public {
+        createTask('Check out DappUniversity.com');
+    }
 }
